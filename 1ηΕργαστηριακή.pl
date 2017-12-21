7a6c0496ad4cc473ee06bd56cde71fb26a3c9650
N Fourtounis <nickfortune@windowslive.com>
2017-12-21 10:40:20 +0200

    depth

--- 1ηΕργαστηριακή.pl
+++ 1ηΕργαστηριακή.pl
@@ -1,3 +1,4 @@
+/*1? ????tµa*/
 edge(a,b).
 edge(a,c).
 edge(a,d).
@@ -17,10 +18,20 @@ edge(d,j).
 edge(j,q).
 edge(j,r).
 edge(j,s).
+/*2? ????tµa*/
 leaf(X) :- not(edge(X,Y)).
+/*3? ????tµa*/
 internal(X) :- edge(X,Y).
+/*4? ????tµa*/
 root(X) :- not(edge(Y,X)).
+/*5? ????tµa*/
 children(X) :- edge(X,Y), write(Y).
+/*6? ????tµa*/
 descendats2(X,Y) :- edge(X,Y),write(Y).
-descendats2(X,Y) :- edge(X,Y), descendats2(Y,Z),write(Y).
+descendats2(X,Y) :- edge(X,Y), descendats2(Y,Z).
 descendats(X) :- descendats2(X,Y).
+/*7? ????tµa*/
+depth2(X,Y,0) :- edge(X,Y).
+depth2(X,Y,D1) :- edge(X,Y), depth2(Z,X,D2), D1 is D2+1.
+depth(X,D) :- depth2(Y,X,D2), D is D2+1.
+/*8? ????tµa*/
--- test.pl
+++ test.pl
@@ -1,6 +1,13 @@
-father(giannis,kostas).
-father(giannis,nikos).
-father(kostas,petros).
-father(kostas,makis).
-father(nikos,spiros).
-brother(X,Y) :- father(Z,X), father(Z,Y), not(X=Y).
+arc(a,b).
+arc(a,c).
+arc(a,d).
+arc(b,e).
+arc(c,e).
+arc(c,f).
+arc(d,f).
+arc(f,g).
+arc(g,e).
+path(X,X).
+path(X,Z) :- arc(X,Y), path(Y,Z).
+path(X,X,0).
+path(X,Z,L1) :- arc(X,Y), path(Y,Z,L2), L1 is L2+1.
